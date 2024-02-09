700796cf63390af9958945682041b3160982c29d
Boris Veis <boris.veis@gmail.com>
2024-02-09 15:55:56 -0800

    provider

--- providers.tf
+++ providers.tf
@@ -7,7 +7,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIATCKASKM7ZCS4PM4X"
-  secret_key = "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"
+  region  = "us-west-1"
 }
