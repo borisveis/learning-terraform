dd4a3b5c27e534ee8da2fd94db2b32ce56b93045
Boris Veis <boris.veis@gmail.com>
2024-02-14 10:53:03 -0800

    attempt to connect to new vpc

--- instances.tf
+++ instances.tf
@@ -1,20 +1,24 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  vpcid = aws_vpc.main.id
 
   tags = {
     Name = "Project VPC"
+
   }
 }
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "Public Subnet CIDR values"
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
 }
 
 variable "private_subnet_cidrs" {
   type        = list(string)
   description = "Private Subnet CIDR values"
   default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
 }
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
@@ -37,7 +41,7 @@ resource "aws_instance" "instance2" {
 }
 resource "aws_subnet" "public_subnets" {
   count      = length(var.public_subnet_cidrs)
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = vpcid
   cidr_block = element(var.public_subnet_cidrs, count.index)
 
   tags = {
@@ -47,7 +51,7 @@ resource "aws_subnet" "public_subnets" {
 
 resource "aws_subnet" "private_subnets" {
   count      = length(var.private_subnet_cidrs)
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = vpcid
   cidr_block = element(var.private_subnet_cidrs, count.index)
 
   tags = {
