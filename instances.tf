34327651b1ce3f4f6fa44ac587799514bedfa372
Boris Veis <boris.veis@gmail.com>
2024-02-14 13:12:37 -0800

    increment instance name-doesn't work

--- instances.tf
+++ instances.tf
@@ -1,31 +1,32 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  vpcid = aws_vpc.main.id
 
   tags = {
     Name = "Project VPC"
-
   }
+}
+variable "index" {
+  type = number
+  default =1
 }
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "Public Subnet CIDR values"
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-
 }
 
 variable "private_subnet_cidrs" {
   type        = list(string)
   description = "Private Subnet CIDR values"
   default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
 }
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
   tags = {
-    Name = "HelloWorld"
+    Name  = var.index + 1
+    Name     = var.index + 1
   }
 }
 resource "aws_instance" "instance2" {
@@ -35,23 +36,22 @@ resource "aws_instance" "instance2" {
   #!/bin/bash -xe
 
   tags = {
-    Name = "instance2"
+    Name  = var.index + 1
 
   }
-}
+  }
 resource "aws_subnet" "public_subnets" {
   count      = length(var.public_subnet_cidrs)
-  vpc_id     = vpcid
+  vpc_id     = aws_vpc.main.id
   cidr_block = element(var.public_subnet_cidrs, count.index)
 
   tags = {
-    Name = "Public Subnet ${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "private_subnets" {
   count      = length(var.private_subnet_cidrs)
-  vpc_id     = vpcid
+  vpc_id     = aws_vpc.main.id
   cidr_block = element(var.private_subnet_cidrs, count.index)
 
   tags = {
